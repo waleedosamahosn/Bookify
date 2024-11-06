f6ac7d1a818ab21ea648481a7aa8cdfb26b607cc
Waleed Osama hosny <Waleed.Osama@ifin-services.com>
2024-11-06 13:15:14 +0200

    Add project files.

---
+++ Bookify.Web/Core/ViewModels/RentalsReportViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookify.Web.Core.ViewModels
+{
+    public class RentalsReportViewModel
+    {
+        public string Duration { get; set; } = null!;
+        public PaginatedList<RentalCopy> Rentals { get; set; }
+    }
+}
